3b5e04cb3277a563e6b156a65ebfdf6d690c81ca
Siddharth <siddharthdhirde99@gmail.com>
2025-03-28 00:08:54 +0530

    add filter for handling all the errors

---
+++ Assessments/inventory-management/src/filters/catch-everything.filter.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+  Catch,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class CatchEverythingFilter implements ExceptionFilter {
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    private readonly logger: Logger,
+  ) {
+    this.logger = new Logger(CatchEverythingFilter.name);
+  }
+
+  catch(exception: any, host: ArgumentsHost): void {
+    this.logger.error(`exception | ${exception}`);
+    if (exception.response)
+      this.logger.error(
+        `exception response: | ${JSON.stringify({ response: exception.response })}`,
+      );
+
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      message: exception.response.message ?? exception.message,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
